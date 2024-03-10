d1a5171c0ac3bddb1697d7480489f7f8a9f16388
waldix04 <152854401+waldix04@users.noreply.github.com>
2024-03-10 21:01:03 +0100

    .

--- php/carexamples/lamborghini-aventador-info.php
+++ php/carexamples/lamborghini-aventador-info.php
@@ -14,10 +14,10 @@
     <main>
         <h1>Lamborghini Aventador</h1>
         <img src="../../assets/images/lamborghini-aventador-ultimae-2021-01.jpg" alt="Lamborghini Aventador" style="width:100%;">
-        <p>Der Lamborghini Aventador repräsentiert das Spitzenmodell der Lamborghini-Reihe. Mit seinem kraftvollen V12-Motor und seiner herausragenden Dynamik bietet der Aventador ein unvergleichliches Fahrerlebnis.</p>
+        <p><pre>Der Lamborghini Aventador repräsentiert das Spitzenmodell der Lamborghini-Reihe. Mit seinem kraftvollen V12-Motor und seiner herausragenden Dynamik bietet der Aventador ein unvergleichliches Fahrerlebnis.</pre></p>
         
         <h3>Spezifikationen</h3>
-        <ul>
+        <ul class="ulLamborghini">
             <li>Motor: 6,5-Liter V12</li>
             <li>Leistung: 740 PS</li>
             <li>Maximales Drehmoment: 690 Nm</li>
@@ -32,7 +32,7 @@
         Ursprünglich war Lamborghini ein erfolgreicher Hersteller von Traktoren und landwirtschaftlichen Maschinen.<br>
         Nach dem Zweiten Weltkrieg begann er jedoch, sein Interesse auf andere Bereiche auszudehnen. Inspiriert von seiner Leidenschaft für schnelle Autos und einer gewissen Enttäuschung über die Qualität der damals führenden Sportwagen, beschloss Lamborghini, sein eigenes Automobilunternehmen zu gründen.<br>
         Im Jahr 1963 wurde Automobili Lamborghini in Sant'Agata Bolognese, Italien, gegründet.<br>
-        Der legendäre Lamborghini Miura, der 1966 auf den Markt kam, war eines der ersten Modelle des Unternehmens und gilt als Pionier der modernen Supersportwagen. Mit seinem bahnbrechenden Design und seiner leistungsstarken Technologie setzte der Miura neue Maßstäbe für Geschwindigkeit und Eleganz auf der Straße.<br>
+        Der legendäre Lamborghini Miura, der 1966 auf den Markt kam, war eines der ersten Modelle des Unternehmens und gilt als Pionier der modernen Supersportwagen. Mit seinem <strong>bahnbrechenden Design und seiner leistungsstarken Technologie</strong> setzte der Miura neue Maßstäbe für Geschwindigkeit und Eleganz auf der Straße.<br>
         In den folgenden Jahren brachte Lamborghini eine Reihe von ikonischen Modellen auf den Markt, darunter den Countach, den Diablo und den Murciélago.<br>
         Jedes dieser Fahrzeuge war ein Meisterwerk der Ingenieurskunst und ein Symbol für Luxus, Leistung und Prestige.<br>
         Trotz finanzieller Herausforderungen und wechselnder Eigentumsverhältnisse blieb Lamborghini ein Vorreiter in der Automobilbranche. Im Jahr 1998 wurde das Unternehmen von der Volkswagen Group übernommen, was zu einer neuen Ära der Innovation und Expansion führte.<br>
